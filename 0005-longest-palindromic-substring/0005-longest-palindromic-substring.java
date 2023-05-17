10d6855241edbfb2d61b6a8a7f86e51827127a21
weonest <98159941+weonest@users.noreply.github.com>
2023-05-17 14:57:00 +0900

    Update 0005-longest-palindromic-substring.java

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,25 +1,31 @@
 class Solution {
     public String longestPalindrome(String s) {
+        
         if (s == null || s.length() < 1) return "";
+        
+        
         int start = 0, end = 0;
+        
         for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = Math.max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
+            int len = Math.max(expand(s, i, i), expand(s, i, i+1)); // 홀수 짝수
+                
+            if (len > end - start) { // 최대 길이 설정
+                start = i - (len - 1) / 2 ;
                 end = i + len / 2;
             }
         }
+        
         return s.substring(start, end + 1);
     }
-
-    private int expandAroundCenter(String s, int left, int right) {
-        int L = left, R = right;
-        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
-            L--;
-            R++;
+    
+    public int expand(String s, int left, int right) {
+        if (s == null || left > right) return 0;
+        
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            
+            left--;
+            right++;
         }
-        return R - L - 1;
+        return right -left - 1;
     }
 }
